3ea7e946a98d8894c726c3b31187c6737e71c48f
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2024-01-08 20:58:07 +0200

    add green container

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue,
+      color: Colors.green,
     );
   }
 }
